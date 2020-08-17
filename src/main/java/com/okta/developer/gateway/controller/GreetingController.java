f460381fa58c0b6d6d5966ecf469f5bf4a7c8781
Eva van Geenen <evavangeenen@gmail.com>
2020-08-17 16:08:09 +0200

    Added requestmapping for greeting endpoint

--- src/main/java/com/okta/developer/gateway/controller/GreetingController.java
+++ src/main/java/com/okta/developer/gateway/controller/GreetingController.java
@@ -6,10 +6,12 @@ import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2Aut
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GreetingController {
 
+    @RequestMapping("/greeting")
     public String greeting(@AuthenticationPrincipal OidcUser oidcUser, Model model,
                            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client){
 
