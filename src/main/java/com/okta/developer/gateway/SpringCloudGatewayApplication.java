2ebe2f1eb58ea8334f7219570be68c8961783b1c
Eva van Geenen <evavangeenen@gmail.com>
2020-08-19 10:22:35 +0200

    Added a route for the cart service in the application class

--- src/main/java/com/okta/developer/gateway/SpringCloudGatewayApplication.java
+++ src/main/java/com/okta/developer/gateway/SpringCloudGatewayApplication.java
@@ -2,7 +2,11 @@ package com.okta.developer.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +14,16 @@ public class SpringCloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder, @Deprecated TokenRelayGatewayFilterFactory filterFactory){
+
+		return builder.routes()
+				.route("cart", r -> r.path("/cart/**")
+				.filters(f-> f.filter(filterFactory.apply()))
+				.uri("lb://cart"))
+				.build();
 	}
 
 }
